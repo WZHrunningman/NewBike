38f9e568b74dc8a1f863bd86cc02813869cd9867
WZHrunningman <2238912552@qq.com>
2019-04-25 00:59:01 +0800

    4.3登录界面，登录未注册用户时，显示“该登录用户不存在”

--- app/src/main/java/com/example/newbike/activity/LoginActivity.java
+++ app/src/main/java/com/example/newbike/activity/LoginActivity.java
@@ -14,11 +14,17 @@ import com.example.newbike.R;
 import com.example.newbike.model.User;
 import com.example.newbike.util.ToastUtil;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
+
+import static com.example.newbike.activity.SignUpActivity.isMobileNO;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -64,25 +70,53 @@ public class LoginActivity extends AppCompatActivity {
     public void onClick() {
         String phone = etPhone.getText().toString().trim();
         String psw = etPsw.getText().toString().trim();
-        User user = new User();
-        user.setUsername(phone);
-        user.setPassword(psw);
-        user.login(new SaveListener<User>() {
+        if (phone.equals("")) {
+            ToastUtil.show("手机号码不能为空");
+            return;
+        }
+        if (!isMobileNO(phone)) {
+            ToastUtil.show("请输入正确的手机号码");
+            return;
+        }
+        if (psw.equals("")) {
+            ToastUtil.show("密码不能为空");
+            return;
+        }
+        BmobQuery<User> query = new BmobQuery<>();
+        query.addWhereEqualTo("username", etPhone.getText().toString().trim());
+        query.findObjects(new FindListener<User>() {
             @Override
-            public void done(User user, BmobException e) {
+            public void done(List<User> list, BmobException e) {
                 if (e == null) {
-                    ToastUtil.show("登录成功");
-                    etPhone.setText("");
-                    etPsw.setText("");
-                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);//跳转到主界面后，并将栈底的Activity全部都销毁
-                    startActivity(intent);
-                    LoginActivity.this.finish();
+                    if (list.size() == 0) {
+                        ToastUtil.show("该登录用戶不存在");
+                    } else {
+                        User user = new User();
+                        user.setUsername(phone);
+                        user.setPassword(psw);
+                        user.login(new SaveListener<User>() {
+                            @Override
+                            public void done(User user, BmobException e) {
+                                if (e == null) {
+                                    ToastUtil.show("登录成功");
+                                    etPhone.setText("");
+                                    etPsw.setText("");
+                                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);//跳转到主界面后，并将栈底的Activity全部都销毁
+                                    startActivity(intent);
+                                    LoginActivity.this.finish();
+                                } else {
+                                    ToastUtil.show("登录失败，用户账号或密码错误");
+                                }
+                            }
+                        });
+                    }
                 } else {
-                    ToastUtil.show("登录失败，用户账号或密码错误");
+                    //查询出错
                 }
             }
         });
+
     }
 
     @OnClick(R.id.tv_SignUp)
